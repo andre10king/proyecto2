c34d0af78816ec24585a917a06a603108a33168e
andres.urrea <andres.urrea@A19-DAP-340>
2023-07-13 14:10:18 -0500

    proyecto con ejecucion en segundo plano

---
+++ ASPWorker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPWorker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ASPWorker/Worker.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ASPWorker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private string _path = Directory.GetCurrentDirectory() + @"\files\";
+        private IFileData _fileData;
+        private int _count = 0;
+        public Worker(ILogger<Worker> logger, IFileData fileData)
+        {
+            _logger = logger;
+            _fileData = fileData;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await _fileData.Create($"{ _path}{ _count++}.txt");
+            }
+        }
+    }
+}
